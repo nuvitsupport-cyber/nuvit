aa7ac858c46e1784e85a28c65e417f5461f5c1d3
nuvitsupport-cyber <nuvit.support@gmail.com>
2026-06-18 17:25:41 +0300

    update web mobile layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1745003851" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3751711774" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
