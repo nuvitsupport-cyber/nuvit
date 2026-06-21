f6a290728c41287cba91f59f53cf2378b76d69a6
nuvitsupport-cyber <nuvit.support@gmail.com>
2026-06-21 16:27:07 +0300

    auto deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2064357988" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2056540918" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
